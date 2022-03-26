0ba7c264d50d87783580f853b9f8055798e5fd3a
joaovianayt <joaoparaty87@gmail.com>
2022-03-26 16:52:15 -0300

    Initial commit

---
+++ AULA 06/AULA 12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 16
+if (idade < 16) {
+    console.log('Não Vota') 
+} else if (idade < 18) {
+    console.log (`Voto Opcional`)
+} else (idade >= 18) {
+   console.log ('Voto Obrigatório')
+}
---
+++ AULA 06/ex008.js
@@ -0,0 +1,10 @@
+var vel = 2
+console.log(`A velocidade do Sue Carro é ${vel}Km/h `)
+if (vel >= 60) { //Condições Simples
+    console.log(`Você ULTRAPASSOU da Velocidade Permitida, MULTADO!`)
+}
+else (vel <= 60)
+{
+    console.log(`Tudo OK, Dirija sempre usando cinto de Segurança!`)
+
+}
